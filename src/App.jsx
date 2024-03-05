5f0a9b4ac11092f5ed1d51cb4ca9c432a1544b4f
Simon Boudara <simon.boudara@etu.minesparis.psl.eu>
2024-03-05 14:46:12 +0100

    try git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div class="flex flex-col justify-around items-center bg-green-200 w-screen h-screen">
+      <div class="w-64 h-64 bg-red-300">
+        <p class="text-4xl text-green-700 text-center py-20">Hello les mines!</p>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
